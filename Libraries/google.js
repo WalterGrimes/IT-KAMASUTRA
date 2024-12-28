3b0493ebd12cd8aeef8c7a35c2eb667519f37426
WalterGrimes <bako.bota2009@gmail.com>
2024-12-28 15:37:38 +0600

    удалил ненужное

--- Libraries/google.js
+++
@@ -1,5 +0,0 @@
-let opemGoogle = () =>{
-  
-}
-
-export default openGoogle;
--- Libraries/maps.js
+++
@@ -1,9 +0,0 @@
-import openGoogle from './google.js';
-import openYandex from './yandex.js';
-
-let openAllMaps = () =>{
-  openGoogle();
-  openYandex();
-}
-
-export default openAllMaps;
